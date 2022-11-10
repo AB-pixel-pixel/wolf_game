4d675f544bac8bc8eb0466b0b9e300eef78ad528
AB-pixel-pixel <ab.pixel.pixel@gmail.com>
2022-11-11 00:08:36 +0800

    完善了PlayerStateManager类

--- app/src/main/java/com/example/atry/PlayerStateManager.java
+++ app/src/main/java/com/example/atry/PlayerStateManager.java
@@ -2,8 +2,6 @@ package com.example.atry;
 
 
 
-import android.util.Log;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,17 +16,17 @@ public class PlayerStateManager {
     public final static Map<Integer,String> int2identity_map = new HashMap<>();
     public final static Map<Integer,String> int2state_map = new HashMap<>();
     // 存放各种数据的列表
-    private static List<civilians> role_list_;
+    public static List<civilians> role_list_;
     private static List<Integer> night_states_;
     public static List<String> visual_player_data_list_;
 
     // 记录游戏状态以及游戏阶段的列表
-    private int game_stage_now_;
+    public static int game_stage_now_;
 
     //游戏阶段，-1未开始游戏，0进入夜晚，1 狼人刀人，2 女巫使用能力，
     // 3 守卫使用能力，4 预言家验身份, 5 遗言，6发言， 7放逐， 8上警
     // 9 狼人自爆 10 猎人使用能力
-    private List<Integer> game_stages_;
+
 
     private int[] date_; // 记录时间，首元素是天数，第二个元素是当天的时间（夜晚，白天）。
 
@@ -47,8 +45,6 @@ public class PlayerStateManager {
 
         // 初始化角色
         init_role_list(identity_list,player_str_list);
-        // 初始化游戏阶段
-        init_game_stage();
         // 初始化文本内容
         init_board_text();
 
@@ -64,19 +60,62 @@ public class PlayerStateManager {
         night_states_.add(-1);
         night_states_.add(-1);
         night_states_.add(-1);
-
+        // 初始化当前游戏阶段
+        game_stage_now_ = -1;
     }// 结束构造函数
 
+
     public void start(){
-
-    }
-
-    public void next(){
-
+        game_stage_now_ = -1;
     }
 
 
+    //---------------------------------------------------------------------------------------------
+    //
+    //      管理UI的可视化内容
+    //
+    //---------------------------------------------------------------------------------------------
 
+
+    /**
+     * 管理公告栏文本
+     * @param game_state
+     * @return 公告栏文本
+     */
+    public String getBoardText(int game_state){
+        return board_text_.get(game_state);
+    }
+
+    //---------------------------------------------------------------------------------------------
+    //
+    //      管理游戏阶段
+    //
+    //---------------------------------------------------------------------------------------------
+
+    public void next_stage(){
+        //让流程在0到5的区间里流动
+        game_stage_now_ = (game_stage_now_ + 1 )%6;
+    }
+
+    public void back_to_daytime_stage(){
+        game_stage_now_ = 5;
+    }
+
+    public int getGame_stage_now_()
+    {
+        return game_stage_now_;
+    }
+
+    public void setGame_stage_now_(int stage)
+    {
+        game_stage_now_ = stage;
+    }
+
+    //---------------------------------------------------------------------------------------------
+    //
+    //      初始化游戏参数的
+    //
+    //---------------------------------------------------------------------------------------------
 
     private void init_translation_map() {
         // 方便数字转为可视化的字符(身份）
@@ -135,7 +174,8 @@ public class PlayerStateManager {
                 }
                 case 3:
                 {
-                    civilians temp = new prophet(i,player_name,identity_num, 1);
+                    // TODO,完善一下这个switch架构，现在懒得改
+                    civilians temp = new civilians(i,player_name,identity_num, 1);
                     role_list_.add(temp);
                     break;
                 }
@@ -159,14 +199,6 @@ public class PlayerStateManager {
         }
     }
 
-    private void init_game_stage(){
-        game_stages_ = new ArrayList<>();
-        for (int i = -1;i<=8;i++)
-        {
-            game_stages_.add(i);
-        }
-        game_stage_now_ = -1;
-    }
 
 
     private void init_board_text(){
@@ -177,25 +209,19 @@ public class PlayerStateManager {
         board_text_.add(temp);
         temp = "狼人请睁眼，指出你要刀的人，狼人请闭眼";
         board_text_.add(temp);
-        temp = "女巫请睁眼，昨晚有人死了，你有一瓶解药，要救吗？你有一瓶毒药要用吗？毒谁？好的，女巫请闭眼";
-        board_text_.add(temp);
         temp = "守卫请睁眼，今晚你守卫哪个人。好的，守卫请闭眼";
+        board_text_.add(temp);
+        temp = "女巫请睁眼。（详情看弹窗）。好的，女巫请闭眼";
         board_text_.add(temp);
         temp ="预言家请睁眼，选择一位玩家查验身份，好人是向上，坏人是向下，他的身份是这个，预言家请闭眼。";
         board_text_.add(temp);
-        temp = "昨晚死的是他，请发表遗言";
+        temp = "白天,由上帝决定各种流程的进展。\n点击'更多'来发动角色的特殊能力、放逐等\n点击'下一阶段'，进入黑夜";
         board_text_.add(temp);
-        temp = "请开始发言。发言完毕，进行公投。";
+        temp = "放逐（点击玩家名称后'确定'则该玩家被放逐，若仅是误触，则点击下一阶段）";
         board_text_.add(temp);
-        temp = "放逐（点击玩家名称后确定则放逐他，若无人被放逐，则点击下一阶段）";
+        temp = "白狼王自爆（点击玩家名称后'确定'则该玩家被带走，若仅是误触，则点击下一阶段）";
         board_text_.add(temp);
-        temp = "竞选警徽的请举手，请发言，没有上警的玩家请投票。最后请警长选择发言顺序";
-        board_text_.add(temp);
-        temp = "白狼王自爆，打断了上警，直接进入黑夜";
-        board_text_.add(temp);
-        temp = "白狼王自爆，打断了发言，直接进入黑夜";
-        board_text_.add(temp);
-        temp = "猎人，你要带走谁";
+        temp = "猎人带走（点击玩家名称后'确定'则该玩家被带走，若仅是误触，则点击下一阶段）";
         board_text_.add(temp);
     }
 
@@ -206,6 +232,11 @@ public class PlayerStateManager {
     }
 }
 
+    //---------------------------------------------------------------------------------------------
+    //
+    //      定义游戏角色
+    //
+    //---------------------------------------------------------------------------------------------
 class civilians{
     // 玩家的序号
     protected int id_;
@@ -213,11 +244,8 @@ class civilians{
     protected String name_;
 
     // 以数字代指身份
-    // "白狼王"->-2 || "狼人"->-1 ||  "平民"->0
-    // "守卫"->1  ||  "猎人"->2  ||  "预言家"->3
-    // "女巫"->4
     protected int identity_;
-    // 存储同索引的玩家角色的生命状态，0 为普通出局， 1位或者， 2为特有能力杀害（毒杀或者白狼王自爆带走）
+    // 存储同索引的玩家角色的生命状态，0 为普通出局， 1为活着， 2为特有能力杀害（毒杀或者白狼王自爆带走）
     protected int state_;
 
     public civilians(int id,String name,int identity,int state)
@@ -227,6 +255,11 @@ class civilians{
         name_ = name;
         identity_ = identity;
         state_ = state;
+    }
+
+    // 定义出局的模式
+    public void out(int mode){
+        state_ = mode;
     }
 
     // 返回可视化的文本数据
@@ -239,6 +272,13 @@ class civilians{
 }
 
 // night_states 用于记录夜晚的情况，分别是，狼人指杀，女巫救药，女巫毒杀，守卫的守护
+
+
+//---------------------------------------------------------------------------------------------
+//
+//      狼人
+//
+//---------------------------------------------------------------------------------------------
 
 interface wolf_ability{
     public void Kill(int target_id,int[] night_states);
@@ -254,6 +294,12 @@ class wolf extends civilians implements wolf_ability{
         night_states[0] = target_id;
     }
 }
+
+//---------------------------------------------------------------------------------------------
+//
+//      女巫
+//
+//---------------------------------------------------------------------------------------------
 
 interface witch_ability{
     public boolean hasPoison();
@@ -279,7 +325,6 @@ class witch extends  civilians implements witch_ability{
 
     @Override
     public void poison(int target_id, int[] night_states) {
-        night_states[1] = -1;
         night_states[2] = target_id;
         poison_number_--;
     }
@@ -292,40 +337,20 @@ class witch extends  civilians implements witch_ability{
     @Override
     public void cure(int target_id, int[] night_states) {
         night_states[1] = target_id;
-        night_states[2] = -1;
         cure_number_--;
     }
 }
 
 
-interface prophet_ability{
-    public int isWolf(int id,List<Integer> id_identity_list, List<Integer> id_states_list);
-}
-
-class prophet extends civilians implements  prophet_ability{
-    prophet (int id,String name,int identity,int state) {
-        super(id, name, identity, state);
-    }
-
-    @Override
-    public int isWolf(int id,List<Integer> id_identity_list, List<Integer> id_states_list) {
-        if (id_states_list.get(id)==1)
-        {
-            if (id_identity_list.get(id)>0)
-            {
-                return 0;
-            }
-            else
-                return 1;
-        }
-        // 人都死了，验不了，要重新验
-        return -1;
-    }
-}
+//---------------------------------------------------------------------------------------------
+//
+//      白狼王
+//
+//---------------------------------------------------------------------------------------------
 
 interface perish_together_skill {
     // 自爆技能
-    public void perish_together(int target_id,List<Integer> id_states_list);
+    public void perish_together(int target_id);
     // 能否使用技能
     public boolean perish_together_condition(int game_states);
 }
@@ -336,17 +361,23 @@ class white_wolf_king extends wolf implements perish_together_skill {
     }
 
     @Override
-    public void perish_together(int target_id,List<Integer> id_states_list) {
-        id_states_list.set(target_id,3);
-        id_states_list.set(this.id_,3);
+    public void perish_together(int target_id) {
+        PlayerStateManager.role_list_.get(target_id).out(3);
+        PlayerStateManager.role_list_.get(target_id).out(3);
     }
 
+    // 检查调用的玩家是不是死了
     @Override
     public boolean perish_together_condition(int game_states) {
-        // game_states 0 为天黑，1为遗言，2为发言，3为放逐，4为上警
-        return game_states == 4 || game_states == 2;
+        return this.state_==1;
     }
 }
+
+//---------------------------------------------------------------------------------------------
+//
+//      猎人
+//
+//---------------------------------------------------------------------------------------------
 
 class hunter extends civilians
 {
@@ -354,51 +385,54 @@ class hunter extends civilians
         super(id, name, identity, state);
     }
 
-    public void perish_together(int target_id, List<Integer> id_states_list) {
-        id_states_list.set(target_id,0);
+    public void perish_together(int target_id) {
+        PlayerStateManager.role_list_.get(target_id).out(0);
     }
 
 
-    public boolean perish_together_condition(int game_states,List<Integer> id_states_list) {
-        if (id_states_list.get(this.id_)==0) // id_state_list 0为普通出局， 1 活着，2 毒死， 3 狼王自爆带走
+    public boolean perish_together_condition(int game_states) {
+        return this.state_==1;
+    }
+
+    @Override
+    public void out(int mode)
+    {
+        this.state_ = mode;
+        if (perish_together_condition(PlayerStateManager.game_stage_now_));
         {
-            if(game_states == 1 || game_states == 3)        // game_states 0 为天黑，1为遗言，2为发言，3为放逐，4为上警
-            {
-                return true;
-            }
+            // TODO 看看UI能怎么和这个类结合再写
+            perish_together_skill(target_id);
         }
-        return false;
     }
 }
 
+//---------------------------------------------------------------------------------------------
+//
+//      守卫
+//
+//---------------------------------------------------------------------------------------------
+
 interface guards_ability{
-    public boolean defend_condition(int target_id,List<Integer> id_states_list);
+    public boolean defend_condition(int target_id);
     public void defend(int target_id,List<Integer> night_states);
 }
 
 class guards extends civilians implements guards_ability{
-    private int last_guard_id;
+    private int last_guard_id_;
     public guards(int id, String name, int identity,int state) {
         super(id, name, identity, state);
-        last_guard_id = -1;
+        last_guard_id_ = -1;
     }
 
     @Override
-    public boolean defend_condition(int target_id, List<Integer> id_states_list) {
-        if(last_guard_id!=target_id)
-        {
-            int states =id_states_list.get(target_id);
-            if (states== 1)
-            {
-                return true;
-            }
-        }
-        return false;
+    public boolean defend_condition(int target_id) {
+        return (last_guard_id_ !=target_id)&&(PlayerStateManager.role_list_.get(target_id).state_ == 0);
     }
 
     @Override
     public void defend(int target_id, List<Integer> night_states) {
         night_states.set(3,target_id);
+        last_guard_id_ = target_id;
     }
 }
 
